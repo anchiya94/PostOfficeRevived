2f78ca70eb28a5cca4c310d0a07a032dfde5179b
anchiya94 <anucm18@gmail.com>
2019-08-15 17:37:54 +0530

    Letter body can have multiple lines

--- src/main/java/com/finzy/postofficerevived/methods/LetterComposer.java
+++ src/main/java/com/finzy/postofficerevived/methods/LetterComposer.java
@@ -11,6 +11,17 @@ import java.util.*;
 
 public class LetterComposer
 {
+    private static void putMetoSleep(int seconds){
+       try        
+       {
+          Thread.sleep(1000*seconds);
+       } 
+       catch(InterruptedException ex) 
+       {
+          Thread.currentThread().interrupt();
+       }
+    }
+    
     private static void getAddress(BufferedReader input,Letter myLetter,String addressType) throws IOException{
        //handle empty input scenarios
        Address myAddress=new Address();
@@ -40,11 +51,12 @@ public class LetterComposer
        int menuChoice = 5;
        ArrayList<Letter> letters = new ArrayList<Letter>();
        int counter = 0;
-       System.out.println("==================================================================");
+       System.out.println("=======================================================================");
        System.out.println("\t\t\tWelcome to Post Office Revived");
-       System.out.println("==================================================================");
+       System.out.println("=======================================================================");
        do {
-           System.out.println("\t\t1. Compose Letter\t2. Review Letter\t3. Edit Letter\t4. Send Letter\t5. I am done");
+           System.out.println("Following are your choices");
+           System.out.println("1. Compose Letter\n2. Review Letter\n3. Edit Letter\n4. Send Letter\n5. I am done");
            try {
                System.out.println("Enter a choice: ");
                menuChoice = Integer.parseInt(input.readLine());
@@ -79,7 +91,7 @@ public class LetterComposer
                StringBuilder letterContent = new StringBuilder(1000);
                String s;
                while ((s = input.readLine()) != null && s.length() != 0) {
-                  letterContent.append ("\n"+s);
+                  letterContent.append (s+"\n");
                }
                myletter.setLetterContent(letterContent.toString());
                letters.add(myletter);
@@ -88,30 +100,115 @@ public class LetterComposer
            }  
            case 2:
            {
-               //handle the case where user asks for a reviewing of letter without actually having a 
-               //letter to review
-               Letter letterToPrint = letters.get(counter-1);
-               printMyLetter(letterToPrint);
-               break;
+               try
+               {
+                  Letter letterToPrint = letters.get(counter-1);
+                  printMyLetter(letterToPrint);
+                  break;
+               }
+               catch(IndexOutOfBoundsException e){
+                  System.out.println("You have no letter to review. Please compose a letter first");
+                  putMetoSleep(1);
+                  break;
+               }
            }
            case 3:
-           {
-               break;
+           {   
+               try{
+                  Letter letterToEdit = letters.get(counter-1);
+                  int editChoice = 5;
+                  System.out.println("Pick a field you want to edit");
+                  do{
+                     System.out.println("1. From Address\n2. To Address\n3. Stamp price\n4. Letter content\n5. Lock the edit and return to previous menu");
+                     try{
+                        System.out.println("Enter a choice: ");
+                        editChoice = Integer.parseInt(input.readLine());
+                     }catch (NumberFormatException e) {
+                        System.out.println("Stick to the choices, please");
+                        continue;
+                     }
+                     switch(editChoice){
+                     case 1:{
+                        System.out.println("From Address-->");
+                        getAddress(input,letterToEdit,"from");
+                        break;
+                     } 
+                     case 2:{
+                        System.out.println("To Address-->");
+                        getAddress(input,letterToEdit,"to");
+                        break;
+                     }
+                     case 3:{
+                        int attempts = 3;
+                        do{
+                           try {
+                              System.out.println("Stamp price:");
+                              int price = Integer.parseInt(input.readLine());
+                              letterToEdit.setStamp(price);
+                              break;
+                           } catch (NumberFormatException e) {
+                              System.out.println("Enter a number!");
+                              //give a message saying these many attempts left
+                              attempts--;
+                     }
+                     }while(attempts>0);
+                        break;
+                     }
+                     case 4:{
+                        System.out.println("Letter content:");
+                        StringBuilder letterContent = new StringBuilder(1000);
+                        String s;
+                        while ((s = input.readLine()) != null && s.length() != 0) {
+                        letterContent.append (s+"\n");
+                        }
+                        letterToEdit.setLetterContent(letterContent.toString());
+                        break;
+                     }
+                     case 5:{
+                        System.out.println("Changes saved");
+                        break;
+                     }
+                     default:{
+                        System.out.println("Enter a valid choice");
+                        putMetoSleep(1); 
+                     }
+                  }
+               }
+               while(editChoice !=5);
+                  break;
+               }
+               catch(IndexOutOfBoundsException ax){
+                  System.out.println("You have no letter to edit. Please compose a letter first");
+                  putMetoSleep(1);
+                  break;  
+               }
+               
            }
            case 4:
            {
-               Letter letterToSend = letters.get(counter-1);
-               PostSender ps = new PostSender(letterToSend);
-               ps.sendPost();
-               break;
+               try{
+                  Letter letterToSend = letters.get(counter-1);
+                  PostSender ps = new PostSender(letterToSend);
+                  ps.sendPost();
+                  putMetoSleep(1);
+                  break;
+               }
+               catch(IndexOutOfBoundsException ax){
+                  System.out.println("You have no letter to send. Please compose a letter first");
+                  putMetoSleep(1);
+                  break;  
+               }
+               
            } 
            case 5:
            {
                System.out.println("Thank you for using Post Office Revived");
+               putMetoSleep(1);
                break;
            }
            default: {
                System.out.println("Enter a valid choice");
+               putMetoSleep(1);
            }
            }
        } while (menuChoice !=5 );
